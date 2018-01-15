3ec2676bc980dbf4358091e0bc1dfc11a3f20266
jiangmang <jiangmang@fingercrm.cn>
2018-01-15 14:13:44 +0800

    eslint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import './css/app.scss'
-
-import '@/global'
-
 import React from 'react'
 import ReactDom from 'react-dom'
+import '@/global'
+
+import './css/app.scss'
+
 import Index from '../src/containers/index'
 
 ReactDom.render(<Index />, document.getElementById('root'))
